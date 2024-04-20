74aee9bad4d64725a0acea8ddd6843772f39cdc4
ANUP KB <anupkb01@gmail.com>
2024-04-20 23:31:02 +0530

    Connected to mongoose as db

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const app = express();
 
+const connectDB = require("./db/connect");
+
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 3001;
+const mongo_uri = process.env.MONGOOSE_URI;
 
 const router = require("./routes/userRoutes");
 app.use(express.json());
 app.use(router);
 
-app.listen(port, () => {
-  console.log(`Server running on: ${port}`);
-});
+const start = async () => {
+  try {
+    await connectDB(mongo_uri);
+    console.log(`Successfully Connected to MongoDB@ ${mongo_uri}`);
+
+    app.listen(port, () => {
+      console.log(`Server Running on Port: ${port}...`);
+    });
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+};
+start();
